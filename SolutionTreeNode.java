9b593f438c4275cc3f50e07444959835e1d2cee7
Apoorva-S-Natesh <104731101+Apoorva-S-Natesh@users.noreply.github.com>
2025-03-12 08:20:24 +0100

    leetcode introduction simple treenode solution

---
+++ SolutionTreeNode.java
@@ -0,0 +1,35 @@
+/*
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+Input: root = [10,4,6]
+Output: true
+Explanation: The values of the root, its left child, and its right child are 10, 4, and 6, respectively.
+10 is equal to 4 + 6, so we return true.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class SolutionTreeNode {
+    public boolean checkTree(TreeNode root) {
+        int sum = root.left.val + root.right.val;
+        if (root.val == sum)
+            return true;
+        return false;
+    }
+}
