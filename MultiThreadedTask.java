44769a550886c744fce0d87e3038a23f40c0d985
Apoorva-S-Natesh <104731101+Apoorva-S-Natesh@users.noreply.github.com>
2025-04-01 06:19:43 +0200

    thread LifeCycle or state

--- MultiThreadedTask.java
+++ MultiThreadedTask.java
@@ -72,3 +72,30 @@ class SavingThread extends Thread
 			
 	}
 }			
+
+/*
+States of threads - thread lifecycle
+1. New - the thread is created but hasn't started working yet, this is where we set the name 
+	Thread thread = new Thread(new MyRunnabe());
+2. Runnable - the thread is ready to run and waiting for its turn
+	thread.start();
+Running - Thread scheduler allots CPU time, thread is running now
+3. Blocked - the thread is waiting to use a resource that's currently in use
+4. Waiting - the thread is waiting for another thread to perform an action
+	synchronized (lock) {
+		lock.wait();
+		//The thread is now in the waiting state
+		}
+	The thread waits until another thread calls notify() or notifyAll()
+5. Timed Waiting - the thread is waiting for a specific amount of time.
+	Thread.sleep(15000);
+
+		or 
+	synchronized (lock) {
+		lock.wait(5000);
+		//thread waits for either notification or 5 seconds
+	}
+ 	thread cannot go to the running directly, once the sleep time is over the thread has to go to the runnable state and when TS allots CPU time, then it can run
+6. Terminated or dead - the thread has finished its task
+from any state can call interrupt(), thread goes to dead state 
+*/
