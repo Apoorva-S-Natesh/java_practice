da68018c89af6069c230a6a0a21efae607a83f8f
Apoorva-S-Natesh <104731101+Apoorva-S-Natesh@users.noreply.github.com>
2025-04-04 07:29:59 +0200

    wait(), notify() for inter thread communication

--- ProducerConsumerProblem.java
+++ ProducerConsumerProblem.java
@@ -19,17 +19,46 @@ class ProducerConsumerProblem
 
 class PizzaShop
 {
-	int token = 1;
+	int token = 0;
+	boolean isReady = false;
 
-	public void bakePizza()
+	public synchronized void bakePizza()
 	{
-		System.out.println("Chef has made pizza number : "+token);
-		token++;
+		if (isReady == false) // If pizza is not ready then make it
+		{
+				token++;
+				System.out.println("Chef has made pizza number : "+token);			
+				isReady = true;
+				notify(); //Notify customer that pizza is ready  	
+		}
+		else
+		{
+			try{
+				wait(); // wait till pizza is consumed to prepare more
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}	
+		
 	}
 
-	public void eatPizza()
+	public synchronized void eatPizza()
 	{
-		System.out.println("Customer has eaten pizza number : "+token);
+		if (isReady)
+		{
+				System.out.println("Customer has eaten pizza number : "+token);
+				isReady = false;
+				notify(); // Notify the chef that pizza has been eaten
+		}
+		else
+		{
+			try
+			{
+				wait(); //thread is in wait state until some other thread calls notify, then the then will be in the runnable state and when Thread scheduler gives the thread time it will go to the running state.
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}	
 	}
 }
 
@@ -46,6 +75,11 @@ class Chef extends Thread
 		while (true)
 		{
 			shop.bakePizza();
+			try {
+				Thread.sleep(1000); //Representing baking time
+			 } catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 			
 	}
@@ -63,11 +97,33 @@ class Customer extends Thread
 		while (true)
 		{
 			shop.eatPizza();
+			try {
+				Thread.sleep(2000); //REpresenting eating time
+			 } catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
 
 /* Output when there is no interThread communication
+
+class PizzaShop
+{
+	int token = 1;
+
+	public void bakePizza()
+	{
+		System.out.println("Chef has made pizza number : "+token);
+		token++;
+	}
+
+	public void eatPizza()
+	{
+		System.out.println("Customer has eaten pizza number : "+token);
+	}
+}
+
 Customer has eaten pizza number : 1
 Customer has eaten pizza number : 1
 Customer has eaten pizza number : 1
