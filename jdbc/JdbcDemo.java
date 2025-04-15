73d56bbea5c5180d90fdfd052d75d3c43f404909
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-15 07:27:29 +0200

    inserting and fetching data

--- jdbc/JdbcDemo.java
+++ jdbc/JdbcDemo.java
@@ -1,20 +1,26 @@
 package com.jdbcDemo;
 
-//1- importing sql package
+//1- importing sql package (my sql connector jar)
+//downladed and added the jar file in buildpath (rightclick project name and then choose libraries, class path, add external jar file )
 import java.sql.*;
 
 public class JdbcDemo {
 
 	public static void main(String[] args) throws Exception{
-		// 2- Loading the driver - downladed and added the jar file in buildpath (rightclick project name and then choose libraries, class path, add external jar file )
+		// 2- Loading the driver - in case of mysql this automatically done, can omit this
+		//type4 driver, Thin driver
 		Class.forName("com.mysql.cj.jdbc.Driver"); 
 		
 		//3- establishing connection
 		String url="jdbc:mysql://localhost:3306/j2ee"; /*if required ip address of system where DB is present instead of localhost : 3306 port where sql is running internally / name of the database
 		can check if the port by typing : <netstat -aon | findstr :3306> on cmd, default for mysql is 3306. then to find which program is suing this port <tasklist /FI "PID eq 2432"> or go to the config file my.ini or my.cnf by typing where my.ini ad check [mysqld] port=3306) */ 
+		
+		/*
+		 *    url:		tech:db://ipaddress or localost:portNumber/database name
+		 */
 		String uname="root";
-		String pass="1234"; //username and password of mysql 
-		Connection con = DriverManager.getConnection(url, uname, pass);
+		String pass="Aj@080123"; //username and password of mysql
+		Connection con = DriverManager.getConnection(url, uname, pass); //establishing the connection, Connection is a interface
 		
 		System.out.println("Connection Established: " + con);
 	}
---
+++ jdbc/JdbcIntroduction.java
@@ -0,0 +1,50 @@
+package com.jdbcDemo;
+
+import java.sql.*;
+
+public class JdbcIntroduction {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String url = "jdbc:mysql://localhost:3306/j2ee";
+		String uname = "root";
+		String pass = "Aj@080123";
+		//String sql = "INSERT INTO student VALUES(7, 'Shashu'),(8, 'Anani')";
+		String sql = "SELECT * FROM student";
+		
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		System.out.println("Connection established to DBMS");
+		
+		//Creating Statement that carries sql query from java to DBMS
+		Statement st = con.createStatement();
+		// st.execute(sql);
+		/*
+		int r = st.executeUpdate(sql);
+		System.out.println(r + " row inserted");
+		*/
+		
+		ResultSet rs = st.executeQuery(sql); //rs will also act like a pointer or iterator 
+		System.out.println("Data received: ");
+		/*
+		rs.next(); //Pointer moves from before the table(or start of table to the first row. If not given, gives the below error
+		System.out.println("Roll: " + rs.getInt(1)); // Row 1, Column 1
+		System.out.println("Name: " + rs.getString(2)); //ROw 1, column 2
+		*/
+		//to print all rows:
+		while(rs.next()) {
+			System.out.print("Roll: " + rs.getInt(1) + " | ");
+			System.out.print("Name: " + rs.getString(2));
+			System.out.println();
+		}
+		
+		/*
+		Error:
+		Exception in thread "main" java.sql.SQLException: Before start of result set
+	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:121)
+	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:89)
+	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:81)
+		 */
+		
+	}
+}
